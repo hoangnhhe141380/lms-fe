b23f2d9aa0a6dc592caa14d25d8d38b6d9ed65ef
Hoang <hoangnh2000.dev@gmail.com>
2022-09-11 17:22:22 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '~/routes'
+
+// Plugins Stylesheet
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Slick Carousel
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+// React Modal Video
+import 'react-modal-video/css/modal-video.min.css'
+
+// StyleSheet
+import '~/assets/css/typography.css'
+import '~/assets/css/shortcodes/shortcodes.css'
+import '~/assets/css/style.css'
+import '~/assets/css/color/color-1.css'
+
+// Fonts
+import '~/assets/vendors/fontawesome/css/font-awesome.min.css'
+import '~/assets/vendors/flaticon/flaticon.css'
+import '~/assets/vendors/line-awesome/css/line-awesome.min.css'
+import '~/assets/vendors/themify/themify-icons.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component
+            return <Route key={index} path={route.path} element={<Page />} />
+          })}
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
