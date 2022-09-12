cd6c6382b56784fc38963153e02ded3ee80f0b5c
Hoang <hoangnh2000.dev@gmail.com>
2022-09-12 21:08:53 +0700

    Update common components

--- src/pages/common/HomePage/index.jsx
+++ src/pages/common/HomePage/index.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 
 import Footer from '~/components/Footer'
 import Header from '~/components/Header'
+import MainSlider from '~/components/MainSlider'
+import ServiceContent from '~/components/ServiceContent'
+import PopularCoursesSlider from '~/components/PopularCoursesSlider'
+import OnlineCoursesCount from '~/components/OnlineCoursesCount'
+import UpcomingEventSlider from '~/components/UpcommingEventSlider'
+import PeopleFeedbackSlider from '~/components/PeopleFeedbackSlider'
+import RecentNewsSlider from '~/components/RecentNewsSliders'
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <div>HomePage</div>
+      <div className="page-content bg-white">
+        <MainSlider />
+        <div className="content-block" id="content-area">
+          <ServiceContent />
+          <PopularCoursesSlider />
+          <OnlineCoursesCount />
+          <UpcomingEventSlider />
+          <PeopleFeedbackSlider />
+          <RecentNewsSlider />
+        </div>
+      </div>
       <Footer />
     </>
   )
